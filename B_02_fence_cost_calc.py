e6a4c7e6639d1a99fa3dd25e9f780d716752f520
jimmyc24942 <jimmyc24942@masseyhigh.school.nz>
2025-05-29 14:33:24 +1200

    working fence cost calculator

---
+++ B_02_fence_cost_calc.py
@@ -0,0 +1,52 @@
+# Ask user for width and loop until they
+# enter a number that is more than zero
+def num_check(question):
+
+    error = "Please enter a number that is more than zero\n"
+    while True:
+
+        try:
+            # ask the user for a number
+            response = float(input(question))
+
+            # check that the number is more than zero
+            if response > 0:
+                return response
+            else:
+                print(error)
+        except ValueError:
+            print(error)
+
+
+# Main route starts here...
+
+keep_going = ""
+while keep_going == "":
+ # Get width and length
+ for item in range (0, 2):
+    width = num_check("Width: ")
+    print(width)
+
+ for item in range(0, 2):
+      length = num_check("Length: ")
+      print(length)
+
+ for item in range(0, 2):
+      cost = num_check("Cost: ")
+      print(cost)
+
+ # Calculate perimeter and price for fence
+ perimeter = 2 * (width + length)
+ price = perimeter * cost
+
+ # Output the area / perimeter
+ print()
+ print(f"Perimeter: {perimeter} meters ")
+ print(f"Price: ${price:.2f}")
+
+ # Ask user if they want to keep going
+ keep_going = input("Press enter to keep going or any key to quit. ")
+ print()
+
+ print("Thank you for using the area / perimeter calculator")
+ print()
